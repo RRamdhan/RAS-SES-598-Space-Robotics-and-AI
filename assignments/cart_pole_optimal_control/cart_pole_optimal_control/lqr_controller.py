ed4ac141eaa548013d3abf19117101926c2d6c9e
RRamdhan <robert.ramdhan@gmail.com>
2025-03-21 23:01:46 +0000

    Update lqr to 30

--- assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
+++ assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
@@ -36,7 +36,8 @@ class CartPoleLQRController(Node):
         ])
         
         # LQR cost matrices
-        self.Q = np.diag([2.0, 2.0, 20.0, 20.0])  # State cost
+        # State vector: [x, ẋ, θ, θ̇]
+        self.Q = np.diag([30.0, 30.0, 300.0, 300.0])  # State cost
         self.R = np.array([[0.1]])  # Control cost
         
         # Compute LQR gain matrix
